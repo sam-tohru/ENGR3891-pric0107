0c5ad678945574c7ac1af15778af3a3d354ca182
Sam <thefrepo@gmail.com>
2019-03-23 16:45:51 +1030

    first attempt at exercise 1

---
+++ unit1-solution1.sh
@@ -0,0 +1,30 @@
+mkdir unit1exercise1
+cd unit1exercise1
+mkdir -p anfahrs/ansprachte
+mkdir -m 451 anfahrs/ansprachte/aufkrauheit 
+mkdir -m 472 anfahrs/ansprachte/bewarfte
+chmod 323 anfahrs/ansprachte
+chmod 006 anfahrs
+mkdir -m 436 angesprachen 
+mkdir -p angegehse/engehheit 
+mkdir -m 445 angegehse/engehheit/einlaufte
+chmod 113 angegehse/engehheit
+chmod 272 angegehse
+mkdir -p angestehte/ansetzer 
+mkdir -m 457 angestehte/ansetzer/angegehse 
+chmod 232 angestehte/ansetzer 
+chmod 060 angestehte 
+mkdir -m 700 belaufheit 
+mkdir -p aufwarfheit/auftrauer 
+mkdir -m 352 aufwarfheit/auftrauer/enkaesst
+chmod 511 aufwarfheit/auftrauer
+chmod 530 aufwarfheit
+mkdir -m 436 angekraust 
+mkdir -p ensitzung/ensitzen 
+mkdir -m 340 ensitzung/ensitzen/behaltst 
+mkdir -m 131 ensitzung/ensitzen/enkatzetest 
+chmod 154 ensitzung/ensitzen
+chmod 230 ensitzung 
+
+cd ..
+sudo tar -zcvf unit1-solution1.tgz unit1exercise1
