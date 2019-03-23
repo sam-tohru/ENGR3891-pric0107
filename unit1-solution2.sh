0e15f0b6fa723705a61e3642d63154cc579035e3
Sam <thefrepo@gmail.com>
2019-03-23 19:34:39 +1030

    exercise 2 first attempt

---
+++ unit1-solution2.sh
@@ -0,0 +1,26 @@
+mkdir unit1exercise2
+cd unit1exercise2
+
+sudo install -d -m 154 -o uucp -g tape anlaufst
+sudo install -d -m 031 -o lp -g mail anpflumst 
+sudo install -d -m 715 -o proxy -g proxy anpflumst/aufsinnte 
+sudo install -d -m 374 -o news -g proxy anpflumst/aufsinnte/aufwarfst 
+sudo install -d -m 612 -o news -g cdrom bekatzekeit 
+sudo install -d -m 145 -o lp -g voice bekatzekeit/angehkeit 
+sudo install -d -m 164 -o nobody -g proxy bekatzekeit/angehunden 
+sudo install -d -m 200 -o student -g mail bekatzekeit/betritttete 
+sudo install -d -m 104 -o mail -g mail bekatzekeit/angehkeit/auspflumer 
+sudo install -d -m 462 -o student -g proxy bekatzekeit/betritttete/angesteher 
+sudo install -d -m 327 -o games -g fax bekatzekeit/betritttete/verschmecken 
+sudo install -d -m 425 -o games -g fax bekatzekeit/angehunden/versinnkeit 
+sudo install -d -m 224 -o uucp -g uucp einkletten 
+sudo install -d -m 537 -o news -g audio zersitzheit 
+sudo install -d -m 232 -o nobody -g mail angerauchst 
+sudo install -d -m 540 -o uucp -g uucp angerauchst/angehalten 
+sudo install -d -m 352 -o games -g voice angerauchst/angehalten/angeraucher 
+sudo install -d -m 563 -o mail -g student angerauchst/angehalten/ausgetrauung 
+sudo install -d -m 121 -o uucp -g dip verrenner 
+sudo install -d -m 243 -o games -g student angehaltkeit 
+
+cd ..
+sudo tar -zcvf unit1-solution2.tgz unit1exercise2
