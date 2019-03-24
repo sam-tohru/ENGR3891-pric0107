4e555823ca76927c1772873e0afc2a5ecbc15405
Sam <thefrepo@gmail.com>
2019-03-24 13:32:43 +1030

    part 4 attempt 1

---
+++ unit1-solution4.sh
@@ -0,0 +1,24 @@
+mkdir unit1exercise4
+cd unit1exercise4
+sudo install -d -m 2157 -o student -g fax bekatzetest
+sudo install -d -m 2341 -o student -g fax einkletttete
+sudo install -d -m 2247 -o student -g voice gerabarbst
+sudo install -d -m 2630 -o student -g tape angesitzte
+sudo install -d -m 001 -o student -g dip auskatzete
+sudo install -d -m 021 -o student -g mail auskatzete/aufstehen
+sudo install -d -m 2142 -o student -g audio auskatzete/aussetzs
+sudo install -d -m 173 -o student -g audio auskatzete/aussetzs/einlaufung
+sudo install -d -m 2335 -o student -g fax auskatzete/aufstehen/auswarfkeit
+sudo install -d -m 071 -o student -g tape auskatzete/aufstehen/ensetzkeit
+sudo install -d -m 2047 -o student -g mail ausgehaltse
+sudo install -d -m 776 -o student -g voice ausgehaltse/zersitzen
+sudo install -d -m 172 -o student -g news ausgehaltse/zersitzen/gehundtest
+sudo install -d -m 2026 -o student -g tape ausgehaltse/zersitzen/ausgesinns
+sudo install -d -m 726 -o student -g cdrom ausgehundtest
+sudo install -d -m 437 -o student -g news ausgehundtest/eingehtete
+sudo install -d -m 624 -o student -g proxy ausgehundtest/eingehtete/enklettheit
+sudo install -d -m 2141 -o student -g cdrom angefahrung
+sudo install -d -m 531 -o student -g tape angefahrung/gerenntete
+sudo install -d -m 2556 -o student -g tape angefahrung/gerenntete/enhundtest
+cd ..
+sudo tar -zcvf unit1-solution4.tgz unit1exercise4
