5b8b3ce3f324c43c9bf38deaf559fe30558efc0d
Sam <thefrepo@gmail.com>
2019-03-24 12:50:21 +1030

    first attempt at number 3 - does not change main unit1exercise3 to be a specific group

---
+++ unit1-solution3.sh
@@ -0,0 +1,24 @@
+mkdir unit1exercise3
+cd unit1exercise3
+sudo install -d -m 036 -o proxy -g audio aufgestehheit
+sudo install -d -m 263 -o nobody -g audio angetraut
+sudo install -d -m 225 -o nobody -g news ansinntete
+sudo install -d -m 376 -o news -g uucp ausgehs
+sudo install -d -m 125 -o news -g fax geschmecktest
+sudo install -d -m 4017 -o mail -g floppy geschmecktest/ausgehaltung
+sudo install -d -m 4326 -o uucp -g proxy geschmecktest/ausgehaltung/behaltheit
+sudo install -d -m 4631 -o uucp -g tape geschmecktest/ausgehaltung/verkrautete
+sudo install -d -m 4161 -o nobody -g uucp bewarfen
+sudo install -d -m 4427 -o student -g uucp bewarfen/gesinnen
+sudo install -d -m 737 -o proxy -g proxy bewarfen/aufsprachtest
+sudo install -d -m 4076 -o nobody -g cdrom bewarfen/gesinnen/einfahrtest
+sudo install -d -m 4053 -o games -g tape bewarfen/aufsprachtest/aufgekaest
+sudo install -d -m 4322 -o nobody -g dip bewarfen/gesinnen/aufkaestete
+sudo install -d -m 710 -o lp -g proxy auftrittung
+sudo install -d -m 377 -o nobody -g proxy auftrittung/angehundung
+sudo install -d -m 175 -o student -g news auftrittung/angehundung/einhalttest
+sudo install -d -m 4545 -o student -g proxy auftrittung/angehundung/verhunds
+sudo install -d -m 254 -o news -g fax verrennheit
+sudo install -d -m 544 -o uucp -g voice verrennheit/ausgetraust
+cd ..
+sudo tar -zcvf unit1-solution3.tgz unit1exercise3
