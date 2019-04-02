32f0b044253d043fdd2ba02c0e9034db68467e4e
Sam <thefrepo@gmail.com>
2019-04-02 14:54:13 +1030

    take 2 of Solution 3 - again after names changed

--- unit1-solution3.sh
+++ unit1-solution3.sh
@@ -1,24 +1,26 @@
 mkdir unit1exercise3
 cd unit1exercise3
-sudo install -d -m 036 -o proxy -g audio aufgestehheit
-sudo install -d -m 263 -o nobody -g audio angetraut
-sudo install -d -m 225 -o nobody -g news ansinntete
-sudo install -d -m 376 -o news -g uucp ausgehs
-sudo install -d -m 125 -o news -g fax geschmecktest
-sudo install -d -m 4017 -o mail -g floppy geschmecktest/ausgehaltung
-sudo install -d -m 4326 -o uucp -g proxy geschmecktest/ausgehaltung/behaltheit
-sudo install -d -m 4631 -o uucp -g tape geschmecktest/ausgehaltung/verkrautete
-sudo install -d -m 4161 -o nobody -g uucp bewarfen
-sudo install -d -m 4427 -o student -g uucp bewarfen/gesinnen
-sudo install -d -m 737 -o proxy -g proxy bewarfen/aufsprachtest
-sudo install -d -m 4076 -o nobody -g cdrom bewarfen/gesinnen/einfahrtest
-sudo install -d -m 4053 -o games -g tape bewarfen/aufsprachtest/aufgekaest
-sudo install -d -m 4322 -o nobody -g dip bewarfen/gesinnen/aufkaestete
-sudo install -d -m 710 -o lp -g proxy auftrittung
-sudo install -d -m 377 -o nobody -g proxy auftrittung/angehundung
-sudo install -d -m 175 -o student -g news auftrittung/angehundung/einhalttest
-sudo install -d -m 4545 -o student -g proxy auftrittung/angehundung/verhunds
-sudo install -d -m 254 -o news -g fax verrennheit
-sudo install -d -m 544 -o uucp -g voice verrennheit/ausgetraust
+sudo install -d -m 615 -o mail -g news aufgehaltse
+sudo install -d -m 053 -o mail -g dip aufgehaltse/ausgesinnst/
+sudo install -d -m 060 -o proxy -g mail aufgehaltse/ausgesinnst/bewitzt/
+sudo install -d -m 676 -o mail -g uucp aufgetrittst
+sudo install -d -m 701 -o student -g floppy aufgetrittst/aufgesetzst/
+sudo install -d -m 4033 -o mail -g audio aufgetrittst/anpflumung/
+sudo install -d -m 4046 -o games -g tape ausfahrs/
+sudo install -d -m 535 -o uucp -g audio aufgehundse/
+sudo install -d -m 4151 -o games -g mail aufgehundse/aufsinnt/
+sudo install -d -m 4171 -o nobody -g dip verkatzes
+sudo install -d -m 4222 -o lp -g floppy angegehs
+sudo install -d -m 4300 -o mail -g news besitzkeit
+sudo install -d -m 322 -o lp -g tape aufgetrittst/aufgesetzst/anhalter/
+sudo install -d -m 352 -o uucp -g voice zerklettse
+sudo install -d -m 4623 -o mail -g fax zerklettse/ausstehtete/
+sudo install -d -m 430 -o mail -g student aufgetrittst/aufgesetzst/gewarftest/
+sudo install -d -m 4503 -o student -g proxy zerklettse/ausstehtete/bespracher
+sudo install -d -m 4632 -o uucp -g dip aufgetrittst/anpflumung/besitzer/
+sudo install -d -m 4725 -o games -g tape aufgetrittst/aufgesetzst/einrabarbkeit/
+sudo install -d -m 4730 -o news -g floppy aufgehaltse/ausgesinnst/eingehse/
 cd ..
+chmod 775 unit1exercise3
 sudo tar -zcvf unit1-solution3.tgz unit1exercise3
+
