e71ff36a1c14e8e3d2591cd784a68758ecdb429b
Sam <thefrepo@gmail.com>
2019-04-02 14:51:59 +1030

    take 2 of unit1 solution 1 (after names where changed from previous week)

--- unit1-solution1.sh
+++ unit1-solution1.sh
@@ -1,31 +1,25 @@
 mkdir unit1exercise1
 cd unit1exercise1
-mkdir -p anfahrs/ansprachte
-mkdir -m 451 anfahrs/ansprachte/aufkrauheit 
-mkdir -m 472 anfahrs/ansprachte/bewarfte
-chmod 323 anfahrs/ansprachte
-chmod 006 anfahrs
-mkdir -m 436 angesprachen 
-mkdir -p angegehse/engehheit 
-mkdir -m 445 angegehse/engehheit/einlaufte
-chmod 113 angegehse/engehheit
-chmod 272 angegehse
-mkdir -p angestehte/ansetzer 
-mkdir -m 457 angestehte/ansetzer/angegehse 
-chmod 232 angestehte/ansetzer 
-chmod 060 angestehte 
-mkdir -m 700 belaufheit 
-mkdir -p aufwarfheit/auftrauer 
-mkdir -m 352 aufwarfheit/auftrauer/enkaesst
-chmod 511 aufwarfheit/auftrauer
-chmod 530 aufwarfheit
-mkdir -m 436 angekraust 
-mkdir -p ensitzung/ensitzen 
-mkdir -m 304 ensitzung/ensitzen/behaltst 
-mkdir -m 131 ensitzung/ensitzen/enkatzetest 
-chmod 154 ensitzung/ensitzen
-chmod 230 ensitzung 
-
+sudo install -d -m 005 -o student -g student angeklettheit/
+sudo install -d -m 012 -o student -g student angeklettheit/gewarfs/ 
+sudo install -d -m 575 -o student -g student zerfahrte/
+sudo install -d -m 277 -o student -g student zerfahrte/ausgegehheit/ 
+sudo install -d -m 065 -o student -g student zerfahrte/ausgegehheit/angekraust/ 
+sudo install -d -m 076 -o student -g student auswitzst/ 
+sudo install -d -m 171 -o student -g student ausgepflumen/
+sudo install -d -m 354 -o student -g student ausgepflumen/aushundkeit/ 
+sudo install -d -m 123 -o student -g student ausgepflumen/aushundkeit/anhaltheit/ 
+sudo install -d -m 151 -o student -g student angeschmecken/ 
+sudo install -d -m 210 -o student -g student verkaesung/ 
+sudo install -d -m 263 -o student -g student aufsprachkeit/ 
+sudo install -d -m 372 -o student -g student angeschmecken/zerstehheit/ 
+sudo install -d -m 407 -o student -g student angeklettheit/gewarfs/ankaesen/ 
+sudo install -d -m 552 -o student -g student angeschmecken/zerstehheit/gesitzer/ 
+sudo install -d -m 566 -o student -g student angeschmecken/auftrittheit/ 
+sudo install -d -m 635 -o student -g student angeschmecken/auftrittheit/ausgeschmeckte/ 
+sudo install -d -m 640 -o student -g student enrauchheit/
+sudo install -d -m 667 -o student -g student ausgepflumen/aushundkeit/aufsteher/ 
+sudo install -d -m 726 -o student -g student zerfahrte/ausgegehheit/angetritten/ 
 cd ..
 chmod 775 unit1exercise1
 sudo tar -zcvf unit1-solution1.tgz unit1exercise1
